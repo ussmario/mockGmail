968366bab52016e59d652a5a2ede5fd76b4697a3
Mario <matthew.davies.3@spaceforce.mil>
2021-10-06 17:59:00 -0400

    barely started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import { GetAllSendersAndSubjects } from './components/GetAllSendersAndSubjects';
+const url = 'http://localhost:3001/';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      results: ['here', 'you', 'go']
+    };
+  }
+
+  async componentDidMount() {
+    let urlEmails = url.concat('emails');
+    let response = await fetch(urlEmails);
+    let dataObj = await response.json();
+    var data = Object.keys(dataObj).map((key) => [key, dataObj[key]]);
+    this.setState({ results: data });
+    this.setState({ katie: data[0][1].sender });
+  }
+
+  render() {
+    const input = document.querySelector('input');
+    if (input) {
+      input.addEventListener('change', (e) => {
+        let searchVal = e.target.value;
+        this.setState({ userInput: searchVal });
+      });
+    }
+
+    return (
+      <div className="App">
+        <GetAllSendersAndSubjects props={this.state.katie} />
+        <form>
+          <button type="button" onClick={GetAllSendersAndSubjects}>View all of my email messages (subject line + sender)</button>
+          <button type="button" onClick={GetAllSendersAndSubjects}>View one of my email messages with all of its details</button>
+          <button type="button" onClick={GetAllSendersAndSubjects}>Send an email</button>
+          <label htmlFor="search">Search for a specific email by subject:</label>
+          <input type="text" placeholder="Enter a subject" name="search" />
+          <button type="button" onClick={GetAllSendersAndSubjects}>Search for a specific email by subject</button>
+        </form>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/GetAllSendersAndSubjects.js
@@ -0,0 +1,10 @@
+export const GetAllSendersAndSubjects = ({props}) => {
+  console.log(props)
+  let ind0 = props;
+
+  return (
+    <div>
+      {ind0}
+    </div>
+  )
+}
